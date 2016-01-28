94008c2d3be014edc851e4e1953f6f37ccb7d574
Ching Wong <ching.wong15@imperial.ac.uk>
2016-01-28 13:05:09 +0000

    28_01_16 13:00

--- src/turtle/Main.java
+++ src/turtle/Main.java
@@ -1,18 +1,20 @@
 package turtle;
 
-import turtle.util.Direction;
+import turtle.util.*;
+/*import turtle.util.Direction;
 import turtle.util.Rotation;
+*/
 
 import java.io.FileNotFoundException;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-    /*  System.out.println("test :");
+     /*System.out.println("test :");
         Direction d = Direction.NORTH;
         d = d.rotate(Rotation.RIGHT, 3);
-        System.out.println("rotated :" + d);
-        */
+     System.out.println("rotated :" + d);
+    */
 
 
 
--- src/turtle/TurtleInterpreter.java
+++ src/turtle/TurtleInterpreter.java
@@ -1,5 +1,84 @@
 package turtle;
 
+import turtle.util.*;
+
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.*;
 
 public class TurtleInterpreter {
+
+  private final Map<String, Turtle> turtles = new HashMap<String, Turtle>();
+  Scanner scanner = new Scanner(System.in);
+  PrintStream print = new PrintStream(System.out);
+  private Paper paper;
+
+
+
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Printing the file passed in:");
+        while(sc.hasNextLine()) System.out.println(sc.nextLine());
+    }
+
+    */
+  public TurtleInterpreter(Scanner scanner, PrintStream print) {
+    this.scanner = scanner;
+    this.print = print;
+    run();
+
+    }
+
+  public void run(){
+
+    while(scanner.hasNext()){
+
+      String line = scanner.nextLine();
+      String[] firstLine = line.split(" ");
+
+        switch(firstLine[0]){
+          case "paper":
+            int width = scanner.nextInt();
+            int heihgt = scanner.nextInt();
+          case "new":
+            if (firstLine[1].equals("normal")){
+              String name = scanner.next();
+              int x = scanner.nextInt();
+              int y = scanner.nextInt();
+            }
+            case "pen":
+              String name = scanner.next();
+              Pen state = Pen.valueOf(scanner.next());
+            case "move":
+              name = scanner.next();
+              int distance = scanner.nextInt();
+            case "right":
+              name = scanner.next();
+              int angle = scanner.nextInt();
+            case "left":
+              name = scanner.next();
+              angle = scanner.nextInt();
+            case "show":
+        }
+
+    }
+
+  }
 }
+
+ /*  if(firstLine[0].equals("paper")){
+
+        }
+
+        if (firstLine[0].equals("new")) {
+          if (firstLine[1].equals("normal")){
+            String name = scanner.next();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+          }
+        }
+      */
+
+// make another method to run and applied on the turtleInterpreter
